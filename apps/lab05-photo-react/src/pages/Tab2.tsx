98c23278734276299d31e808d52c510030d1f82d
yodsanonDK <pLoomN321654987@gmail.com>
2026-01-18 20:27:39 +0700

    Lab5

---
+++ apps/lab05-photo-react/src/pages/Tab2.tsx
@@ -0,0 +1,62 @@
+import { camera } from 'ionicons/icons';
+// CHANGE: Update import
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/react';
+import { usePhotoGallery } from '../components/usePhotoGallery';
+
+const Tab2: React.FC = () => {
+  const { photos, addNewToGallery } = usePhotoGallery();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle size="small">Lab 05 - โดย ยศนนท์ ดวงไข รหัส 663380021-5</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+            <IonTitle size="small">Lab 05 - โดย ยศนนท์ ดวงไข รหัส 663380021-5</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+
+        {/* CHANGE: Add the floating action button */}
+       <IonGrid>
+          <IonRow>
+            {/* CHANGE: Create a new column and image component for each photo */}
+            {photos.map((photo) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => addNewToGallery()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+
+export default Tab2;
